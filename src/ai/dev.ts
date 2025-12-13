cf23b9cfa3d11890f732b6631b80a9865cb628e5
Ass Gaye <assgaye83@gmail.com>
2025-12-13 20:43:56 +0000

    Cannot find module for page: route not found /page

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/explain-waec-question.ts';
+import '@/ai/flows/explain-waec-question';
 import '@/ai/flows/simplify-explanation-on-confusion.ts';
 import '@/ai/flows/generate-waec-quiz.ts';
 import '@/ai/flows/transcribe-audio';
